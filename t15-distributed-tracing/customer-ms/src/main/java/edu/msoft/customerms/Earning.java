3f8315c7126abb477d8a58b9838f1069f95d55d9
Fausto Almeida <fausto.almeidac@gmail.com>
2022-08-10 17:50:54 -0500

    Actualizando modelo de Earnings en taller 15

---
+++ t15-distributed-tracing/customer-ms/src/main/java/edu/msoft/customerms/Earning.java
@@ -0,0 +1,44 @@
+package edu.msoft.customerms;
+
+import java.math.BigDecimal;
+
+public class Earning {
+
+    private Long customerId;
+    private Short year;
+    private BigDecimal amount;
+
+    public Earning () {
+    }
+
+    public Earning (Long customerId, Short year, BigDecimal amount) {
+        this.customerId = customerId;
+        this.year = year;
+        this.amount = amount;
+    }
+
+    public Long getCustomerId () {
+        return customerId;
+    }
+
+    public void setCustomerId (Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Short getYear () {
+        return year;
+    }
+
+    public void setYear (Short year) {
+        this.year = year;
+    }
+
+    public BigDecimal getAmount () {
+        return amount;
+    }
+
+    public void setAmount (BigDecimal amount) {
+        this.amount = amount;
+    }
+
+}
