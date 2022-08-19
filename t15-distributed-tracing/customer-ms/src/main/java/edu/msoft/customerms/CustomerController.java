4ef4736f223bb67329b54d1cda4677496db59085
Fausto Almeida <fausto.almeidac@gmail.com>
2022-08-19 14:04:48 -0500

    Adding log lines taller 15

--- t15-distributed-tracing/customer-ms/src/main/java/edu/msoft/customerms/CustomerController.java
+++ t15-distributed-tracing/customer-ms/src/main/java/edu/msoft/customerms/CustomerController.java
@@ -1,5 +1,7 @@
 package edu.msoft.customerms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,8 @@ public class CustomerController {
 
     private final IRSClient irsClient;
 
+    private static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
     public CustomerController (CustomerRepository customerRepository,
                                @Value("${customer.some-property}") String someProperty,
                                IRSClient irsClient) {
@@ -30,16 +34,19 @@ public class CustomerController {
 
     @PostMapping("/customer")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        logger.info("Creating customer with name {}", customer.getName());
         return new ResponseEntity<>(this.customerRepository.save(customer), HttpStatus.CREATED);
     }
 
     @GetMapping("/customers")
     public ResponseEntity<List<Customer>> getAllCustomers() {
+        logger.info("Getting all customers");
         return new ResponseEntity<>(this.customerRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/customer/{id}")
     public ResponseEntity<Customer> getCustomerByID(@PathVariable Long id) {
+        logger.info("Getting customer with id {}", id);
         return this.customerRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
@@ -47,16 +54,20 @@ public class CustomerController {
     
     @GetMapping("/customer/property")
     public ResponseEntity<String> getCustomerProperty() {
+        logger.info("Getting customer test property: {}", this.someProperty);
         return new ResponseEntity<>(this.someProperty, HttpStatus.OK);
     }
 
     @GetMapping("/customer/pid")
     public ResponseEntity<String> getPID() {
+        logger.info("Getting customer PID: {}", System.getProperty("PID"));
         return new ResponseEntity<>(System.getProperty("PID"), HttpStatus.OK);
     }
 
     @GetMapping("/customer/{customerId}/full-history")
     public ResponseEntity<CustomerFullHistory> getCustomerFullHistory (@PathVariable Long customerId) {
+
+        logger.info("Getting customer full history");
 
         Optional<Customer> customer = this.customerRepository.findById(customerId);
 
