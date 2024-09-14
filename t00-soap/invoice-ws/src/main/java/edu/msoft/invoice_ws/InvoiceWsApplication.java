e4f34ecc10faf934d2380bb5d1f99b00b6464327
Fausto Almeida <fausto.almeidac@gmail.com>
2024-09-14 00:05:39 -0500

    Adding t00

---
+++ t00-soap/invoice-ws/src/main/java/edu/msoft/invoice_ws/InvoiceWsApplication.java
@@ -0,0 +1,13 @@
+package edu.msoft.invoice_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceWsApplication.class, args);
+	}
+
+}
---
+++ t00-soap/invoice-ws/src/test/java/edu/msoft/invoice_ws/InvoiceWsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.msoft.invoice_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
