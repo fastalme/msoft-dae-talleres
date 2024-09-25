04bc831ab836989f877e425fd7da39b890071c58
Fausto Almeida <fausto.almeidac@gmail.com>
2024-09-24 23:13:45 -0500

    Adding working WSDLs

--- t00-soap/invoice-ws/src/main/java/edu/msoft/invoice/InvoiceWsApplication.java
+++ t00-soap/invoice-ws/src/main/java/edu/msoft/invoice/InvoiceWsApplication.java
@@ -1,4 +1,4 @@
-package edu.msoft.invoice_ws;
+package edu.msoft.invoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- t00-soap/invoice-ws/src/test/java/edu/msoft/invoice/InvoiceWsApplicationTests.java
+++ t00-soap/invoice-ws/src/test/java/edu/msoft/invoice/InvoiceWsApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.msoft.invoice_ws;
+package edu.msoft.invoice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
